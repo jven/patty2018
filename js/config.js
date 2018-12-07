e4d864675b362f8569d0bd975208f52ebe033e68
Justin Venezuela <jven@jvenezue.la>
2018-12-06 19:01:16 -0500

    Reduces the block hysteresis.

--- js/config.js
+++ js/config.js
@@ -40,7 +40,7 @@ const Config = {
   GIFT_VICTORY_SCALE: 0.2,
 
   /** The time needed for Patty to touch a block before it moves. */
-  BLOCK_HYSTERESIS: 20,
+  BLOCK_HYSTERESIS: 15,
   /** The time it takes for the block to move one tile. */
   BLOCK_MOVE_DURATION: 200,
   /**
